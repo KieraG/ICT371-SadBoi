ce93d5db5ade6ae808af1963a7766309b393f5e5
albedoDeveloper <kyehorbury@msn.com>
2020-04-12 16:40:00 +0800

    Added the Scene 3-1 and 3-2 worlds

---
+++ Assets/AllowContinue.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllowContinue : MonoBehaviour
+{
+    public GameObject blocker;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.Q))
+        {
+            blocker.GetComponent<Transition>().continueOn = true;
+
+        }
+    }
+    
+}
