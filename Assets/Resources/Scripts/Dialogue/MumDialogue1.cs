7d32157969c64f0b731ab9fed8815e23c3afd103
albedoDeveloper <kyehorbury@msn.com>
2020-04-22 19:49:28 +0800

    Added mum dialogue

---
+++ Assets/Resources/Scripts/Dialogue/MumDialogue1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MumDialogue1 : DialogueInteractableInterface
+{
+    private bool hadConversation = false;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public override void TriggerAction()
+    {
+        if (!hadConversation)
+        {
+
+            dialogueManager.Enqueue("I say stuff here");
+
+            hadConversation = true;
+        }
+        else
+        {
+            dialogueManager.Enqueue("I say stuff here after you have talked to me!");
+        }
+    }
+}
