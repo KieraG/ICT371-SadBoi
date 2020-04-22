6a218f50a6b98cff006cc109c5055a730e1c9764
albedoDeveloper <kyehorbury@msn.com>
2020-04-22 19:43:19 +0800

    Added all dialogue and fixed clipping issues

--- Assets/Kye Assets/Scripts/Transition.cs
+++ Assets/Kye Assets/Scripts/Transition.cs
@@ -35,7 +35,6 @@ public class Transition : MonoBehaviour
         if (col.gameObject.tag == "Player" && continueOn)
         {
             busTransition.gameObject.SetActive(true);
-            Debug.Log("hello");
             StartCoroutine(FadeInTime());
         }
 
---
+++ Assets/Resources/Scripts/Dialogue/OnAwakeThreeOne.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnAwakeThreeOne : MonoBehaviour
+{
+    public DialogueManager mang;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mang.Enqueue("Good Friend: Hey, you finally made it! This is the third scene in the life of the main character. Here we are in the middle of the city, protesting against the governments lack of action on reducing the effect on climate change! (Press E to contunue)");
+        mang.Enqueue("Good Friend: You can hear the chants of all the protestors, its crazy, hopefully it leads to some good changes!");
+        mang.Enqueue("Good Friend: We will then go to talk about the different ways you can take action against climate change to help push the change in government policy, and what you personally can do about it, in the full game!");
+        mang.Enqueue("Good Friend: I almost forgot! I lost our other friend within the crowd! Can you help me find them! Remember they always wear their shoes with the purple shoelaces! (This person randomly appears in the game, meaning his hiding location on the map is different each time!");
+        mang.Enqueue("Press E to interact with our friend when you find them!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
