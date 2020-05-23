8127920d02bf8bb65fbdadf39d6382d89e3bdb8b
Peter <petercrabbe92@gmail.com>
2020-05-23 20:58:05 +0800

    Add filler to world

---
+++ Assets/Resources/Scripts/Dialogue/MumDialog2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MumDialog2 : DialogueInteractableInterface
+{
+    private bool hadConversation = false;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public override void TriggerAction()
+    {
+        if (!hadConversation)
+        {
+            dialogueManager.Enqueue("Mum: Your still playing games?");
+            dialogueManager.Enqueue("Mum: Its way passed your bedtime...");
+            dialogueManager.Enqueue("Mum: Did you even bother to atleast watch the video that was on.");
+            dialogueManager.Enqueue("Mum: Anyway head to bed.");
+            hadConversation = true;
+        }
+        else
+        {
+            dialogueManager.Enqueue("Come on, its way past your bedtime.");
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/PlayerChase.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayerChase : MonoBehaviour
+{
+
+    public Transform player;
+    public int viewDistance = 100000;
+    public float speed = 0.01f;
+    public float multiplier = 0.0001f;
+
+    void Start()
+    {
+        player = GameObject.Find("Player").transform;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    void Update()
+    {
+        
+        if (Vector3.Distance(player.position, this.transform.position) < viewDistance) //if player comes within a distance less than 10, Apex activates
+        {
+            Vector3 direction = player.position - this.transform.position;
+
+            direction.y = 0.0f;
+
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
+                            Quaternion.LookRotation(direction), 0.1f); //same as above note. not sure what went wrong
+
+            this.GetComponent<Rigidbody>().AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.Impulse);
+        }
+
+    }
+
+}
+
