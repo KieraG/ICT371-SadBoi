7aec182ee675b5cd1018dbaa67cd6c6087218b06
Christo Stephenson <christo@blueskylabs.com.au>
2020-05-25 14:12:53 +0800

    scene 4 - finished basic layout of top floor, add broken elevators, add staircase

---
+++ Assets/Christo Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    public bool open = false;
+
+    public Vector3 openAngle = new Vector3(0, 270, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnTriggerStay(Collider collision)
+    {
+        Debug.Log("OnTriggerStay");
+        Debug.Log(collision.gameObject.tag);
+        if (collision.gameObject.tag == "Player" && (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown("joystick button 2")) && open)
+        {
+            Debug.Log("close");
+            transform.Rotate(new Vector3(0, 0, 0));
+            open = false;
+        }
+
+        else if (collision.gameObject.tag == "Player" && (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown("joystick button 2")) && !open)
+        {
+            Debug.Log("open");
+            transform.Rotate(openAngle);
+            open = true;
+        }
+
+    }
+}
