5b7e328d7b300621cbdb3dd4c6a7616cdd3a6ff7
Peter <petercrabbe92@gmail.com>
2020-06-04 01:19:41 +0800

    Add missing Lonk

---
+++ Assets/Resources/Scripts/Game1Dialog.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game1Dialog : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool hadConversation = false;
+    public DialogueManager dialogueManager;
+    void Start()
+    {
+        if (!hadConversation)
+        {
+            dialogueManager.Enqueue("Welcome to Chasebot5000");
+            dialogueManager.Enqueue("See that ball over there...");
+            dialogueManager.Enqueue("He is coming for you!!!.");
+            dialogueManager.Enqueue("RUUUUUUUUUUUUUUUUUUUUUUN");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!dialogueManager.DialogQueued())
+        {
+            hadConversation = true;
+        }
+    }
+}
+
