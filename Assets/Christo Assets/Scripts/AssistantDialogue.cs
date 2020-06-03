375f1a959aa38318e720bedda5dcf8148548790a
Christo Stephenson <christocs@iinet.net.au>
2020-06-03 23:34:05 +0800

    update

---
+++ Assets/Christo Assets/Scripts/AssistantDialogue.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Dialogue that lets the user talk to their mum
+// The first conversation is different to the following conversations
+public class AssistantDialogue : DialogueInteractableInterface
+{
+    // Whether the user has already had a conversation with the NPC
+    private bool hadConversation = false;
+    
+    private GameObject scene4State = null;
+
+    void Start()
+    {
+        scene4State = GameObject.Find("State");
+    }
+    
+    // Determines what to do when the action is triggered
+    public override void TriggerAction()
+    {
+        if (!hadConversation) {
+            Debug.Log("trigger action");
+            dialogueManager.Enqueue("Secretary: Good morning boss, how can I help?");
+            dialogueManager.Enqueue("Me: What tasks have I got at hand?");
+            dialogueManager.Enqueue("Secretary: Your employees need your help around the office, then you've got an interview with a potential recruit.");
+            dialogueManager.Enqueue("Secretary: William is just around the corner, please check with him first.");
+            hadConversation = true;
+            scene4State.GetComponent<Scene4State>().arrowState = "William";
+        } else {
+            dialogueManager.Enqueue("Secretary: Your employees need your help around the office, then you've got an interview with a potential recruit.");
+        }
+    }
+}
---
+++ Assets/Christo Assets/Scripts/Scene4State.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class Scene4State : MonoBehaviour
+{
+    private GameObject waypoint = null;
+    public string arrowState = "SecretaryPerson";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waypoint = GameObject.Find("Waypoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (arrowState.Length > 0)
+        {
+            waypoint.GetComponent<Waypoint>().SetPosition(GameObject.Find(arrowState).transform.position);
+        } else
+        {
+            waypoint.GetComponent<Waypoint>().SetPosition(new Vector3(-1000, -1000, -1000));
+        }
+    }
+}
