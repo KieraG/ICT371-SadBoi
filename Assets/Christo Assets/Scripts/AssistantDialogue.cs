7218893a546ac442f781ff79d855dea7a8384265
Christo Stephenson <christocs@iinet.net.au>
2020-06-04 07:24:13 +0800

    added more dialogue and fetch quest

--- Assets/Christo Assets/Scripts/AssistantDialogue.cs
+++ Assets/Christo Assets/Scripts/AssistantDialogue.cs
@@ -12,27 +12,34 @@ public class AssistantDialogue : DialogueInteractableInterface
 
     public bool gaveAirconJob = false;
     public bool airconJobFinished = false;
-    
+
+    public bool gaveSteveJob = false;
+
     private GameObject scene4State = null;
     public WilliamInteraction william = null;
+    public SteveInteraction steve = null;
 
     void Start()
     {
         scene4State = GameObject.Find("State");
     }
-    
+
     // Determines what to do when the action is triggered
     public override void TriggerAction()
     {
-        if (!gaveWilliamJob) {
+        if (!gaveWilliamJob)
+        {
             dialogueManager.Enqueue("Secretary: Good morning boss, how can I help?");
             dialogueManager.Enqueue("Me: What tasks have I got at hand?");
-            dialogueManager.Enqueue("Secretary: Your employees need your help around the office, then you've got an interview with a potential recruit.");
+            dialogueManager.Enqueue(
+                "Secretary: Your employees need your help around the office, then you've got an interview with a potential recruit.");
             dialogueManager.Enqueue("Secretary: William is just around the corner, please check with him first.");
             gaveWilliamJob = true;
             william.jobGiven = true;
             scene4State.GetComponent<Scene4State>().arrowState = "William";
-        } else {
+        }
+        else
+        {
             if (!williamJobFinished)
             {
                 dialogueManager.Enqueue("Secretary: I believe William still needs your help.");
@@ -42,12 +49,16 @@ public class AssistantDialogue : DialogueInteractableInterface
                 if (!gaveAirconJob)
                 {
                     dialogueManager.Enqueue("Me: What's the run down on repairing the office?");
-                    dialogueManager.Enqueue("Secretary: We haven't found anyone available to fix the elevator or kitchen appliances yet.");
-                    dialogueManager.Enqueue("Secretary: The air conditioner was booked to be repaired yesterday, though the repairman never showed up.");
-                    dialogueManager.Enqueue("Secretary: Little irresponsible considering they took the whole system apart, though I hear they're quite busy.");
+                    dialogueManager.Enqueue(
+                        "Secretary: We haven't found anyone available to fix the elevator or kitchen appliances yet.");
+                    dialogueManager.Enqueue(
+                        "Secretary: The air conditioner was booked to be repaired yesterday, though the repairman never showed up.");
+                    dialogueManager.Enqueue(
+                        "Secretary: Little irresponsible considering they took the whole system apart, though I hear they're quite busy.");
                     dialogueManager.Enqueue("Me: The aircon needs to be fixed ASAP, its getting unbearably hot.");
                     dialogueManager.Enqueue("Me: I'll take a shot at repairing it myself");
-                    dialogueManager.Enqueue("Secretary: You will need to reassemble the vents in the room next door by picking up vents and placing them down.");
+                    dialogueManager.Enqueue(
+                        "Secretary: You will need to reassemble the vents in the room next door by picking up vents and placing them down.");
                     dialogueManager.Enqueue("Secretary: Use the interact key to pick up/drop vents");
                     dialogueManager.Enqueue("Secretary: Let me know if you need any help");
                     scene4State.GetComponent<Scene4State>().arrowState = "";
@@ -57,16 +68,35 @@ public class AssistantDialogue : DialogueInteractableInterface
                 {
                     if (!airconJobFinished)
                     {
-                        dialogueManager.Enqueue("Secretary: Reassemble the vents in the room next door by picking up vents and placing them down.");
+                        dialogueManager.Enqueue(
+                            "Secretary: Reassemble the vents in the room next door by picking up vents and placing them down.");
                         dialogueManager.Enqueue("Secretary: Use the interact key to pick up/drop vents");
                     }
                     else
                     {
-                        dialogueManager.Enqueue("Secretary: Good job boss");
+                        if (!gaveSteveJob)
+                        {
+                            dialogueManager.Enqueue("Secretary: Good job boss.");
+                            scene4State.GetComponent<Scene4State>().arrowState = "Steve";
+                            gaveSteveJob = true;
+                            steve.jobGiven = true;
+                        }
+
+                        if (!steve.jobComplete)
+                        {
+                            dialogueManager.Enqueue(
+                                "Secretary: Steve wants some help downstairs, please provide him with some assistance.");
+                            dialogueManager.Enqueue(
+                                "Secretary: After you help Steve, make sure to interview the potential recruit.");
+                        }
+                        else
+                        {
+                            dialogueManager.Enqueue(
+                                "Secretary: Make sure to interview the potential recruit.");
+                        }
                     }
                 }
             }
         }
     }
-
 }
