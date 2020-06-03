38a592a40183c9413b0827039e72965e85da3758
Christo Stephenson <christocs@iinet.net.au>
2020-06-04 06:05:32 +0800

    added credits, added backdrop, made background darker to make text easier to read

---
+++ Assets/Resources/Scripts/CreditsMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsMenu : MonoBehaviour
+{
+    public GameObject mainMenu;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainMenu.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+    public void ToggleMainMenu()
+    {
+        mainMenu.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
--- Assets/Resources/Scripts/MainMenu.cs
+++ Assets/Resources/Scripts/MainMenu.cs
@@ -5,16 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject credits;
+
     void Start()
     {
         // Make sure the cursor is visible for the main menu scene
         Cursor.lockState = CursorLockMode.None;
+        credits.SetActive(false);
     }
 
     // Changes the scene to scene 1 when called, should be called externally (e.g. by a UI button)
     public void PlayScene()
     {
         SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+
+    public void ToggleCredits()
+    {
+        credits.SetActive(true);
+        gameObject.SetActive(false);
     }
 
     // Quits the program when called, should be called externally (e.g. by a UI button)
